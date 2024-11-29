631dbe9ab5abdea840613e5ef8ed4c9d90194f3e
Daniel Hufschläger <Daniel.Hufschlaeger@blha.brandenburg.de>
2024-11-29 07:42:16 +0100

    fixing project.toml.

--- design_pattern/cli.py
+++ design_pattern/cli.py
@@ -7,7 +7,6 @@ def main():
     parser.add_argument("-i", "--input", help="input csv file (with path)", type=str, required=True)
     parser.add_argument("-o", "--out", help="output xml file (with path)", type=str, required=True)
 
-
     try:
         args = parser.parse_args()
         xmlbuidler: DroidCsvXmlBuilder = DroidCsvXmlBuilder(args.input, args.out)
